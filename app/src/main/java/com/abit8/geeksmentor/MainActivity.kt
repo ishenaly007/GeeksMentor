fd1fb516a952741cca832d37dcec17878dc22e45
ishenaly007 <ishennarkozuev07@gmail.com>
2023-05-04 19:13:46 +0300

    Initial commit

---
+++ app/src/main/java/com/abit8/geeksmentor/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.abit8.geeksmentor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.abit8.geeksmentor.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        /*supportFragmentManager.beginTransaction().add(R.id.fragment_container, OnBoardFragment1()).commit()
+
+        // Создаем ViewPager2 и адаптер для фрагментов
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        // Фрагменты для онбординга
+        class OnboardingFragment1 : Fragment(R.layout.fragment_on_board1)
+        class OnboardingFragment2 : Fragment(R.layout.fragment_on_board2)
+        class OnboardingFragment3 : Fragment(R.layout.fragment_on_board3)
+
+        // Добавляем три фрагмента в адаптер
+        val fragment1 = OnboardingFragment1()
+        val fragment2 = OnboardingFragment2()
+        val fragment3 = OnboardingFragment3()
+        adapter.addFragment(fragment1)
+        adapter.addFragment(fragment2)
+        adapter.addFragment(fragment3)*/
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/abit8/geeksmentor/ui/auth/PasswordForgetFragment.kt
@@ -0,0 +1,19 @@
+package com.abit8.geeksmentor.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.abit8.geeksmentor.R
+
+class PasswordForgetFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password_forget, container, false)
+    }
+}
