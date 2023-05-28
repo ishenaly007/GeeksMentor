8889bd4bcc8319d6f5a79159d8a0a887ae1a43f3
aianakend <ayanakend@gmail.com>
2023-05-28 11:56:25 +0600

    +some adds

--- app/src/main/java/com/abit8/geeksmentor/ui/mentorprofile/ProfileFragment.kt
+++ app/src/main/java/com/abit8/geeksmentor/ui/mentorprofile/ProfileFragment.kt
@@ -5,18 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.abit8.geeksmentor.databinding.FragmenProfileBinding
+import com.abit8.geeksmentor.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var binding: FragmenProfileBinding
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmenProfileBinding.inflate(inflater, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
