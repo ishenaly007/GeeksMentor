0a5aff5a23056a9dac95a9c113aede782f290a61
itishn1k <kayrat1224mamataev@gmail.com>
2023-06-30 15:37:51 +0600

    111не совсем готово, надо пагинацию добавить и переделать под апишку

---
+++ app/src/main/java/com/abit8/geeksmentor/MentorMapper.kt
@@ -0,0 +1,35 @@
+//package com.abit8.geeksmentor
+//
+//import com.abit8.geeksmentor.data.model.MentorDto
+//import com.abit8.geeksmentor.domain.model.Mentor
+//
+//object MentorMapper {
+//    fun mapToDomain(mentorDto: MentorDto): Mentor {
+//        return Mentor(
+//            address = mentorDto.address,
+//            age = mentorDto.age,
+//            birthDate = mentorDto.birthDate,
+//            bloodGroup = mentorDto.bloodGroup,
+//            domain = mentorDto.domain,
+//            ein = mentorDto.ein,
+//            email = mentorDto.email,
+//            eyeColor = mentorDto.eyeColor,
+//            firstName = mentorDto.firstName,
+//            gender = mentorDto.gender,
+//            height = mentorDto.height,
+//            id = mentorDto.id,
+//            image = mentorDto.image,
+//            ip = mentorDto.ip,
+//            lastName = mentorDto.lastName,
+//            macAddress = mentorDto.macAddress,
+//            maidenName = mentorDto.maidenName,
+//            password = mentorDto.password,
+//            phone = mentorDto.phone,
+//            ssn = mentorDto.ssn,
+//            university = mentorDto.university,
+//            userAgent = mentorDto.userAgent,
+//            username = mentorDto.username,
+//            weight = mentorDto.weight
+//        )
+//    }
+//}
